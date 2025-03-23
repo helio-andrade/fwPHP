f160f26690ba58afbeaccda4aaeabb93993bd6c4
Hélio Andrade <helio_andrade@hotmail.com.br>
2025-03-23 17:37:27 -0300

    Update exemplo-2-1.php

--- system/exemplos/exemplo-2-1.php
+++ system/exemplos/exemplo-2-1.php
@@ -4,21 +4,15 @@
  * and its descendants
  */
 
-// Ajuste o caminho conforme sua estrutura de pastas
 include_once(__DIR__ . '/../../system/classes/Database.php');
 
-// Cria uma instância da classe MySQL
 $db = new MySQL();
 
-// Configurações de conexão
 $db->setHost('localhost');
 $db->setDatabase('siteweb');
 $db->setUsername('root');
 $db->setPassword('root');
 $db->setPort(3306);
 
-// Conecta e exibe informações sobre a conexão
 // var_dump($db->connect());
 print_r($db->connect());
-
-echo '<br />Deu certo?!';
